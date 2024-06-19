06976e3a6c21a0073774cb890a824009ee02f5e9
PedroDaAlanis <pmtex06@gmail.com>
2024-06-18 23:33:16 -0300

    hamood

--- mobile-navbar.js
+++ mobile-navbar.js
@@ -1,17 +1,6 @@
 const itens = document.getElementById('itens')
 function apareceMenu(){
-//     if(itens.style.display == 'block'){
-//         itens.style.display = 'none'
-//     }else{
-//         itens.style.display = 'block'
-//     }
-// }   
-// function ajustarMenu() {
-//     if (window.innerWidth >= 770) {
-//         itens.style.display = 'block'; 
-//     } else if (itens.style.display != 'none') {
-//         itens.style.display = 'none'; 
-//     }
+
     if( itens.style.transform == "translateX(100%)"){
     itens.style.transform= "translateX(0)"
     }
@@ -20,7 +9,13 @@ function apareceMenu(){
     }
  }
 
-// window.addEventListener('resize', ajustarMenu);
+ function resetMenu() {
+    const itens = document.getElementById('itens');
+    if (window.innerWidth > 770) { 
+        itens.style.transform = "translateX(0)";
+    } else {
+        itens.style.transform = "translateX(100%)"; // Ou o estado desejado para telas menores
+    }
+}
 
-// // Garantir que o estado inicial esteja correto
-// ajustarMenu();
+window.addEventListener('resize', resetMenu);
