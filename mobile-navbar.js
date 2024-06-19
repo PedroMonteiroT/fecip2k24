68d4e4aa84ef9af6b8f68a50789358dc5392bd63
PedroDaAlanis <pmtex06@gmail.com>
2024-06-18 22:36:19 -0300

    ai messiiasisiaiiai

--- mobile-navbar.js
+++ mobile-navbar.js
@@ -1,19 +1,25 @@
 const itens = document.getElementById('itens')
 function apareceMenu(){
-    if(itens.style.display == 'block'){
-        itens.style.display = 'none'
-    }else{
-        itens.style.display = 'block'
+//     if(itens.style.display == 'block'){
+//         itens.style.display = 'none'
+//     }else{
+//         itens.style.display = 'block'
+//     }
+// }   
+// function ajustarMenu() {
+//     if (window.innerWidth >= 770) {
+//         itens.style.display = 'block'; 
+//     } else if (itens.style.display != 'none') {
+//         itens.style.display = 'none'; 
+//     }
+    if( itens.style.transform == "translateX(100%)"){
+    itens.style.transform= "translateX(0)"
     }
-}   
-function ajustarMenu() {
-    if (window.innerWidth >= 770) {
-        itens.style.display = 'block'; 
-    } else if (itens.style.display != 'none') {
-        itens.style.display = 'none'; 
+    else{
+        itens.style.transform= "translateX(100%)"
     }
-}
-window.addEventListener('resize', ajustarMenu);
+ }
+// window.addEventListener('resize', ajustarMenu);
 
-// Garantir que o estado inicial esteja correto
-ajustarMenu();
+// // Garantir que o estado inicial esteja correto
+// ajustarMenu();
