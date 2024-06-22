49541def0f08632f3f44eb351c430ac84cff47d4
PedroDaAlanis <pmtex06@gmail.com>
2024-06-21 22:41:46 -0300

    é

---
+++ index.js
@@ -0,0 +1,5 @@
+let rad = document.getElementById('alergia')
+let text = document.getElementById('alergias')
+let rad2 = document.getElementById('deficiencia')
+let text2 = document.getElementById('deficiencias')
+
