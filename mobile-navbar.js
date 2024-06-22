f091a28e93f5bf0989773159558c3a01d67670c4
PedroDaAlanis <pmtex06@gmail.com>
2024-06-21 22:00:09 -0300

    plmns 1

--- mobile-navbar.js
+++ mobile-navbar.js
@@ -1,21 +1,42 @@
-const itens = document.getElementById('itens')
-function apareceMenu(){
-
-    if( itens.style.transform == "translateX(100%)"){
-    itens.style.transform= "translateX(0)"
-    }
-    else{
-        itens.style.transform= "translateX(100%)"
-    }
- }
-
- function resetMenu() {
-    const itens = document.getElementById('itens');
-    if (window.innerWidth > 770) { 
-        itens.style.transform = "translateX(0)";
-    } else {
-        itens.style.transform = "translateX(100%)"; // Ou o estado desejado para telas menores
-    }
+const itens = document.getElementById("itens");
+function apareceMenu() {
+  if (itens.style.transform == "translateX(100%)") {
+    itens.style.transform = "translateX(0)";
+  } else {
+    itens.style.transform = "translateX(100%)";
+  }
 }
 
-window.addEventListener('resize', resetMenu);
+function resetMenu() {
+  if (window.innerWidth > 770) {
+    itens.style.transform = "translateX(0)";
+  } else {
+    itens.style.transform = "translateX(100%)";
+  }
+}
+
+window.addEventListener("resize", resetMenu);
+
+// window.addEventListener("scroll", function () {
+//   if (window.scrollY > 100) {
+//     document.getElementById("header").style.backgroundColor = "black";
+//   } else {
+//     document.getElementById("header").style.backgroundColor = "";
+//   }
+// });
+let lastScrollTop = 0;
+const header = document.querySelector('header')
+
+window.addEventListener("scroll", function () {
+  let scrollTop = window.scrollY;
+  if (window.scrollY > 70) {
+    if (scrollTop > lastScrollTop) {
+      // Rolando para baixo
+      header.style.top = "-100px"; // Ajuste a altura de acordo com a altura do seu cabeçalho
+    } else {
+      // Rolando para cima
+      header.style.top = "0";
+    }
+  }
+  lastScrollTop = scrollTop;
+});
---
+++ opcoesdeadm.js
@@ -0,0 +1,24 @@
+function mostraropcoes(element) {
+  const opcoesmesmo = element.nextElementSibling;
+  if (opcoesmesmo.style.width === "200px") {
+    opcoesmesmo.style.width = "0";
+    opcoesmesmo.style.height = "0";
+  } else {
+    opcoesmesmo.style.width = "200px";
+    opcoesmesmo.style.height = "230px";
+  }
+}
+
+// Fechar todos os menus ao clicar fora deles
+document.addEventListener("click", function (event) {
+  const opcoesmesmoList = document.querySelectorAll(".opcoesmesmo");
+  opcoesmesmoList.forEach(function (opcoesmesmo) {
+    if (
+      !opcoesmesmo.contains(event.target) &&
+      !opcoesmesmo.previousElementSibling.contains(event.target)
+    ) {
+      opcoesmesmo.style.width = "0";
+      opcoesmesmo.style.height = "0";
+    }
+  });
+});
