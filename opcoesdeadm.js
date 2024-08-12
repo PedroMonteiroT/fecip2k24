6983d18104432005adc3ecad4c9524be620b245f
PedroDaAlanis <pmtex06@gmail.com>
2024-08-12 00:44:03 -0300

    VAPOVAPO

--- opcoesdeadm.js
+++ opcoesdeadm.js
@@ -1,11 +1,33 @@
+// function mostraropcoes(element) {
+//   const opcoesmesmo = element.nextElementSibling;
+//   if (opcoesmesmo.style.width === "200px") {
+//     opcoesmesmo.style.width = "0";
+//     opcoesmesmo.style.height = "0";
+//   } else {
+//     opcoesmesmo.style.width = "200px";
+//     opcoesmesmo.style.height = "230px";
+//   }
+// }
+
+// // Fechar todos os menus ao clicar fora deles
+// document.addEventListener("click", function (event) {
+//   const opcoesmesmoList = document.querySelectorAll(".opcoesmesmo");
+//   opcoesmesmoList.forEach(function (opcoesmesmo) {
+//     if (
+//       !opcoesmesmo.contains(event.target) &&
+//       !opcoesmesmo.previousElementSibling.contains(event.target)
+//     ) {
+//       opcoesmesmo.style.width = "0";
+//       opcoesmesmo.style.height = "0";
+//     }
+//   });
+// });
 function mostraropcoes(element) {
   const opcoesmesmo = element.nextElementSibling;
-  if (opcoesmesmo.style.width === "200px") {
-    opcoesmesmo.style.width = "0";
-    opcoesmesmo.style.height = "0";
+  if (opcoesmesmo.style.transform === "scale(1)") {
+    opcoesmesmo.style.transform = "scale(0)";
   } else {
-    opcoesmesmo.style.width = "200px";
-    opcoesmesmo.style.height = "230px";
+    opcoesmesmo.style.transform = "scale(1)";
   }
 }
 
@@ -17,8 +39,7 @@ document.addEventListener("click", function (event) {
       !opcoesmesmo.contains(event.target) &&
       !opcoesmesmo.previousElementSibling.contains(event.target)
     ) {
-      opcoesmesmo.style.width = "0";
-      opcoesmesmo.style.height = "0";
+      opcoesmesmo.style.transform = "scaleX(0)";
     }
   });
 });
